b7f32baaf3f21ca10590433cac78038c8b39fa7d
jimmyruann <jimmyruan@hotmail.com>
2022-06-02 14:10:32 +1000

    fix typo and export

---
+++ src/dt/tenant/index.ts
@@ -0,0 +1,32 @@
+export * as AlertingProfiles from "./settings/AlertingProfiles";
+export * as ApplicationDetectionRules from "./settings/ApplicationDetectionRules";
+export * as AutoTaggingRules from "./settings/AutoTaggingRules";
+export * as CalcServiceMetrics from "./settings/CalcServiceMetrics";
+export * as Extensions from "./settings/Extensions";
+export * as HostNamingRules from "./settings/HostNamingRules";
+export * as ManagementZones from "./settings/ManagementZones";
+export * as ManualTaggingRules from "./settings/ManualTaggingRules";
+export * as MonitoringOverview from "./settings/MonitoringOverview";
+export * as ProblemIntegrations from "./settings/ProblemIntegrations";
+export * as ProcessNamingRules from "./settings/ProcessNamingRules";
+export * as RequestAttributes from "./settings/RequestAttributes";
+export * as RequestNamingRules from "./settings/RequestNamingRules";
+export * as ServiceNamingRules from "./settings/ServiceNamingRules";
+
+export * as AWSIntegrations from "./integrations/AWSIntegrations";
+export * as AzureIntegrations from "./integrations/AzureIntegrations";
+export * as KubernetesIntegrations from "./integrations/KubernetesIntegrations";
+export * as VMWareIntegrations from "./integrations/VMWareIntegrations";
+
+export * as ApplicationBasicSettings from "./applications/ApplicationBasicSettings";
+export * as ApplicationConversionGoals from "./applications/ApplicationConversionGoals";
+export * as ApplicationSessionProperties from "./applications/ApplicationSessionProperties";
+export * as ApplicationUserActionRules from "./applications/ApplicationUserActionRules";
+export * as ApplicationUserActions from "./applications/ApplicationUserActions";
+export * as ApplicationUserTags from "./applications/ApplicationUserTags";
+export * as Applications from "./applications/Applications";
+
+// export * as Dashboards from "./environment/Dashboards"
+export * as MultidimensionalCharts from "./environment/MultidimensionalCharts";
+export * as SLO from "./environment/SLO";
+export * as Synthetics from "./environment/Synthetics";
